f6dd3b003aa428fb2772c2cc135a2b9392d891a0
zurajoestar <cristinal2@learn.cssd.ab.ca>
2018-11-13 14:59:12 -0700

    commit with error

--- PhidgetApp/ViewController.swift
+++ PhidgetApp/ViewController.swift
@@ -7,14 +7,190 @@
 //
 
 import UIKit
+import Phidget22Swift
 
 class ViewController: UIViewController {
 
+    let allQuestions = QuestionBank()
+    var pickedAnswer : Bool = false
+    var questionNumber : Int = 0
+    var score : Int = 0
+    
+    //for phidget
+    let buttonArray = [DigitalInput(), DigitalInput()]
+    let ledArray = [DigitalOutput(), DigitalOutput()]
+    
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    func attach_handler(sender: Phidget){
+        do{
+            
+            let hubPort = try sender.getHubPort()
+            
+            if(hubPort == 0){
+                print("Button 0 Attached")
+            }
+            else if (hubPort == 1){
+                print("Button 1 Attached")
+            }
+            else if (hubPort == 2){
+                print("LED 2 Attached")
+            }
+            else{
+                print("LED 3 Attached")
+            }
+            
+        } catch let err as PhidgetError{
+            print("Phidget Error " + err.description)
+        } catch{
+            //catch other errors here
+        }
+    }
+    
+    func state_change_button0(sender:DigitalInput, state:Bool){
+        do{
+            if(state == true){
+                print("Button Pressed")
+                try ledArray[0].setState(true)
+                pickedAnswer = true
+                checkAnswer()
+                nextQuestion()
+            }
+            else{
+                print("Button Not Pressed")
+                
+            }
+        } catch let err as PhidgetError {
+            print("Phidget Error " + err.description)
+        } catch{
+            //catch other errors here
+        }
+    }
+    
+    func state_change_button1(sender:DigitalInput, state:Bool){
+        do{
+            if(state == true){
+                print("Button Pressed")
+                try ledArray[0].setState(true)
+                pickedAnswer = false
+                checkAnswer()
+                nextQuestion()
+            }
+            else{
+                print("Button Not Pressed")
+                
+            }
+        } catch let err as PhidgetError {
+            print("Phidget Error " + err.description)
+        } catch{
+            //catch other errors here
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let firstQuestion = allQuestions.list[0]
+        questionLabel.text = firstQuestion.questionText
+        
+        updateUI()
+        
+        do {
+            //enable server
+            try Net.enableServerDiscovery(serverType: .deviceRemote)
+            
+            //address, add handler, open digital BUTTONS
+            for i in 0..<buttonArray.count{
+                try buttonArray[i].setDeviceSerialNumber(528040)
+                try buttonArray[i].setHubPort(i)
+                try buttonArray[i].setIsHubPortDevice(true)
+                let _ = buttonArray[i].attach.addHandler(attach_handler)
+                try buttonArray[i].open()
+            }
+            
+            
+            //address, add handler, open LEDs
+            for i in 0..<ledArray.count{
+                try ledArray[i].setDeviceSerialNumber(528040)
+                try ledArray[i].setHubPort(i+2)
+                try ledArray[i].setIsHubPortDevice(true)
+                let _ = ledArray[i].attach.addHandler(attach_handler)
+                try ledArray[i].open()
+            }
+            
+            let _ = buttonArray[0].stateChange.addHandler(state_change_button0)
+            let _ = buttonArray[1].stateChange.addHandler(state_change_button1)
+            
+            
+            
+        } catch let err as PhidgetError {
+            print("Phidget Error " + err.description)
+        } catch {
+            //other errors here
+        }
     }
 
 
+    func updateUI() {
+        
+        scoreLabel.text = "Score: \(score)"
+        
+    }
+    
+    
+    func nextQuestion() {
+        
+        if questionNumber <= 12 {
+            questionLabel.text = allQuestions.list[questionNumber].questionText
+            
+            updateUI()
+        }
+        else {
+            
+            let alert = UIAlertController(title: "Great job!", message: "You have finished the quiz! Do you want to start over?", preferredStyle: .alert)
+            
+            let restartAction = UIAlertAction (title: "Restart", style: .default, handler: { (UIAlertAction) in
+                self.startOver()
+            })
+            
+            
+            alert.addAction(restartAction)
+            
+            present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    
+    func checkAnswer() {
+        
+        let correctAnswer = allQuestions.list[questionNumber].answer
+        
+        if correctAnswer == pickedAnswer {
+            
+            ProgressHUD.showSuccess("Correct!")
+            
+            score = score + 1
+        }
+            
+        else {
+            ProgressHUD.showError("Wrong!")
+        }
+        
+    }
+    
+    
+    func startOver() {
+        
+        score = 0
+        questionNumber = 0
+        nextQuestion()
+        
+    }
+    
+    
+    
 }
+
+
 
